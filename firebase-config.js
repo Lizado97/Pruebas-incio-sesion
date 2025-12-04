82d7012aec8c6c1298e0d436eaa57eaf04db812d
Lizado97 <95575824+Lizado97@users.noreply.github.com>
2025-12-04 19:49:33 +0100

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCMDdNr6wB6gZe00olBid3oi-c9FuLR27c",
+  authDomain: "loguin-f1921.firebaseapp.com",
+  projectId: "loguin-f1921",
+  storageBucket: "loguin-f1921.firebasestorage.app",
+  messagingSenderId: "726207211540",
+  appId: "1:726207211540:web:395370c11e864151301f01",
+  measurementId: "G-D4HF5PVS64"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
